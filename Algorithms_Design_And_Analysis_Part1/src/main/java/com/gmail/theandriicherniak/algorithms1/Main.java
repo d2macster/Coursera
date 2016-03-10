18df8c6e5093557f59839e7f8793115ba6660f6f
Andrii Cherniak <theandriicherniak@gmail.com>
2016-03-10 13:51:47 -0800

    SCC

--- Algorithms_Design_And_Analysis_Part1/src/main/java/com/gmail/theandriicherniak/algorithms1/Main.java
+++ Algorithms_Design_And_Analysis_Part1/src/main/java/com/gmail/theandriicherniak/algorithms1/Main.java
@@ -9,56 +9,40 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        String dataFile = "/Users/andriicherniak/Desktop/kargerMinCut.txt";
+        String dataFile = "/Users/andriicherniak/Desktop/SCC.txt";
         File file = new File(dataFile);
-        BufferedReader reader = null;
+        BufferedReader reader;
 
-        Random rand = new Random(System.currentTimeMillis());
+        HashMap<Integer, ArrayList<Integer>> graph_original = new HashMap<Integer, ArrayList<Integer>>();
+        HashMap<Integer, ArrayList<Integer>> graph_rev = new HashMap<Integer, ArrayList<Integer>>();
 
-
-        HashMap<Integer, ArrayList<Integer>> graph_connectivity = new HashMap<Integer, ArrayList<Integer>>();
+        int from, to;
 
         try {
             reader = new BufferedReader(new FileReader(file));
-            String line = null;
+            String line;
 
             while ((line = reader.readLine()) != null) {
-                ArrayList<Integer> v_set = new ArrayList<Integer>();
-                String[] v_ar = line.split("\\s+");
+                String [] v_ar = line.split("\\s+");
 
-                for (int i = 1; i < v_ar.length; i++) {
-                    v_set.add(Integer.parseInt(v_ar[i]));
-                }
+                from = Integer.parseInt(v_ar[0]);
+                to = Integer.parseInt(v_ar[1]);
 
-                graph_connectivity.put(Integer.parseInt(v_ar[0]), v_set);
+                if (!graph_original.containsKey(from)) graph_original.put(from, new ArrayList<Integer>());
+                graph_original.get(from).add(to);
+
+                if (!graph_rev.containsKey(to)) graph_rev.put(to, new ArrayList<Integer>());
+                graph_rev.get(to).add(from);
             }
 
         } catch (Exception e) {
-            System.out.println(e.getLocalizedMessage());
+            System.out.println(e.getMessage());
         }
 
-        HashMap<Integer, ArrayList<Integer>> graph_copy = new HashMap<Integer, ArrayList<Integer>>();
-        int minCutValue = graph_connectivity.keySet().size();
+        System.out.println("extracted data");
 
-        for (int j = 1; j <= 1000; j++) {
 
-            for (int v : graph_connectivity.keySet()){
-                ArrayList<Integer> vv = new ArrayList<Integer>();
-                for (int to : graph_connectivity.get(v)){
-                    vv.add(to);
-                }
-                graph_copy.put(v, vv);
-            }
-
-            HW3_MinCut minCut = new HW3_MinCut(graph_copy, rand);
-            int [] result = minCut.contraction();
-
-            if (result.length != 2) System.out.println("error");
-            if (result[0] != result[1]) System.out.println("error");
-
-            minCutValue = Math.min(minCutValue, result[0]);
-        }
-
-        System.out.println(minCutValue);
+        HW3_SCC scc = new HW3_SCC(graph_original, graph_rev);
+        scc.computeSCC();
     }
 }
