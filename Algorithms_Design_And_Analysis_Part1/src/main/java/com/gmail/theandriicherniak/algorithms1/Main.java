f24030a7b36d22fe89319923182aa2e0b6ef1940
Andrii Cherniak <theandriicherniak@gmail.com>
2016-03-14 12:33:53 -0700

    AVL tree

--- Algorithms_Design_And_Analysis_Part1/src/main/java/com/gmail/theandriicherniak/algorithms1/Main.java
+++ Algorithms_Design_And_Analysis_Part1/src/main/java/com/gmail/theandriicherniak/algorithms1/Main.java
@@ -13,56 +13,44 @@ public class Main {
         File file = new File(dataFile);
         BufferedReader reader;
 
-        HashMap<Integer, HashMap<Integer, Integer>> graph_weights = new HashMap<Integer, HashMap<Integer, Integer>>();
 
-        try {
-            reader = new BufferedReader(new FileReader(file));
-            String line;
-
-            while ((line = reader.readLine()) != null) {
-                String [] v_ar = line.split("\\s+");
-                int from = Integer.parseInt(v_ar[0]);
-
-                graph_weights.put(from, new HashMap<Integer, Integer>());
-
-                for (int i = 1; i < v_ar.length; i++){
-                    String [] link_data = v_ar[i].split(",");
-                    int to = Integer.parseInt(link_data[0]);
-                    int weight = Integer.parseInt(link_data[1]);
-
-                    graph_weights.get(from).put(to, weight);
-                }
-
-            }
-
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
-        System.out.println("extracted data");
-
-        HW4_Dijkstra d = new HW4_Dijkstra(graph_weights);
-        d.run();
-
-//        class IntComparator implements Comparator<Integer>{
-//            public int compare(Integer v1, Integer v2){
-//                if (v1 > v2) return 1;
-//                if (v1 < v2) return -1;
-//                return 0;
+//        try {
+//            reader = new BufferedReader(new FileReader(file));
+//            String line;
+//
+//            while ((line = reader.readLine()) != null) {
+//                String [] v_ar = line.split("\\s+");
+//                int from = Integer.parseInt(v_ar[0]);
+//
+//                graph_weights.put(from, new HashMap<Integer, Integer>());
+//
+//                for (int i = 1; i < v_ar.length; i++){
+//                    String [] link_data = v_ar[i].split(",");
+//                    int to = Integer.parseInt(link_data[0]);
+//                    int weight = Integer.parseInt(link_data[1]);
+//
+//                    graph_weights.get(from).put(to, weight);
+//                }
+//
 //            }
+//
+//        } catch (Exception e) {
+//            System.out.println(e.getMessage());
 //        }
 //
-//        MinPriorityQueue q = new MinPriorityQueue(100, new IntComparator());
-//        for (int i = 1; i <= 150; i++) {
-//            q.add(3 * i % 10);
-////            q.show();
-//        }
-//
-//
-//        while (!q.isEmpty()){
-//            System.out.println(q.remove());
-////            q.show();
-//        }
+//        System.out.println("extracted data");
+
+
+        Random rnd = new Random();
+        BalancedBST bst = new BalancedBST();
+        for (int i = 0; i <= 100; i ++) bst.insert(i);
+
+        System.out.println("max " + bst.max() + " pred " + bst.pred(bst.max()));
+        System.out.println("min " + bst.min() + " succ " + bst.succ(bst.min()));
+        System.out.println(" is balanced : " + bst.is_balanced());
+
+
+        bst.show();
 
 
     }
--- Algorithms_Design_And_Analysis_Part1/src/main/java/com/gmail/theandriicherniak/algorithms1/MinPriorityQueue.java
+++ Algorithms_Design_And_Analysis_Part1/src/main/java/com/gmail/theandriicherniak/algorithms1/MinPriorityQueue.java
@@ -98,5 +98,4 @@ public class MinPriorityQueue<T> {
             return null;
         }
     }
-
 }
