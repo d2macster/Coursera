f4a6e876de0692d55d0ea774b3b969695c23b9f5
Andrii Cherniak <theandriicherniak@gmail.com>
2016-04-05 12:21:21 -0700

    two sum : speed up hash solution

--- Algorithms_Design_And_Analysis_Part1/src/main/java/com/gmail/theandriicherniak/algorithms1/Main.java
+++ Algorithms_Design_And_Analysis_Part1/src/main/java/com/gmail/theandriicherniak/algorithms1/Main.java
@@ -5,52 +5,29 @@ package com.gmail.theandriicherniak.algorithms1;
  */
 
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        String dataFile = "/Users/andriicherniak/Desktop/dijkstraData.txt";
+        String dataFile = "/Users/andriicherniak/Desktop/Median.txt";
         File file = new File(dataFile);
         BufferedReader reader;
 
+        long [] data = new long[1000000];
 
-//        try {
-//            reader = new BufferedReader(new FileReader(file));
-//            String line;
-//
-//            while ((line = reader.readLine()) != null) {
-//                String [] v_ar = line.split("\\s+");
-//                int from = Integer.parseInt(v_ar[0]);
-//
-//                graph_weights.put(from, new HashMap<Integer, Integer>());
-//
-//                for (int i = 1; i < v_ar.length; i++){
-//                    String [] link_data = v_ar[i].split(",");
-//                    int to = Integer.parseInt(link_data[0]);
-//                    int weight = Integer.parseInt(link_data[1]);
-//
-//                    graph_weights.get(from).put(to, weight);
-//                }
-//
-//            }
-//
-//        } catch (Exception e) {
-//            System.out.println(e.getMessage());
-//        }
-//
-//        System.out.println("extracted data");
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String line;
+            int id = 0;
 
+            while ((line = reader.readLine()) != null) {
+                data[id] = Long.parseLong(line);
+                id ++;
+            }
 
-        Random rnd = new Random();
-        BalancedBST bst = new BalancedBST();
-        for (int i = 0; i <= 100; i ++) bst.insert(i);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
-        System.out.println("max " + bst.max() + " pred " + bst.pred(bst.max()));
-        System.out.println("min " + bst.min() + " succ " + bst.succ(bst.min()));
-        System.out.println(" is balanced : " + bst.is_balanced());
-
-
-        bst.show();
 
 
     }
