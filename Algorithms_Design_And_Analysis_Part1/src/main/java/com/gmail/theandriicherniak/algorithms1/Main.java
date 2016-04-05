1b918eacacb3e5389f09790323c5c4ea3e04158c
Andrii Cherniak <theandriicherniak@gmail.com>
2016-04-05 13:25:44 -0700

    median maintenance

--- Algorithms_Design_And_Analysis_Part1/src/main/java/com/gmail/theandriicherniak/algorithms1/Main.java
+++ Algorithms_Design_And_Analysis_Part1/src/main/java/com/gmail/theandriicherniak/algorithms1/Main.java
@@ -5,6 +5,7 @@ package com.gmail.theandriicherniak.algorithms1;
  */
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,21 +13,28 @@ public class Main {
         File file = new File(dataFile);
         BufferedReader reader;
 
-        long [] data = new long[1000000];
+        MedianMaintenance mm = new MedianMaintenance(10000);
+        ArrayList<Integer> median = new ArrayList<Integer>();
 
         try {
             reader = new BufferedReader(new FileReader(file));
             String line;
-            int id = 0;
 
             while ((line = reader.readLine()) != null) {
-                data[id] = Long.parseLong(line);
-                id ++;
+                int v = Integer.parseInt(line);
+                median.add(mm.addValue(v));
             }
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+
+        int result = 0;
+        for (int m : median){
+            result = (result + m) % 10000;
+        }
+
+        System.out.println(result);
 
 
 
